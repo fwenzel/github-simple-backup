7f2d46e493b43038f040b91b796695922d2cfecf
Jeff Balogh <me@jeffbalogh.org>
2009-12-19 11:05:15 -0800

    sorting by repo name, showing description, json-powered

--- github-simple-backup.py
+++ github-simple-backup.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
+import json
 import os
-import os.path
 from subprocess import call
 import sys
 import urllib
-from xml.etree.ElementTree import ElementTree
 
 
 # API URL to grab repository list
-REPO_LIST_API = 'http://github.com/api/v2/xml/repos/show/%(username)s'
+REPO_LIST_API = 'http://github.com/api/v2/json/repos/show/%(username)s'
 # github clone URL
 CLONE_URL = 'git://github.com/%(username)s/%(repo_name)s.git'
 
@@ -17,14 +16,15 @@ def usage():
     print "Usage: ./github-simple-backup.py username /path/to/backup/\n"
     sys.exit()
 
-def clone_or_pull(username, backup_dir, repo_name):
-    print "Backing up %s..." % repo_name
-    repo_dir = '/'.join([backup_dir, repo_name])
+def clone_or_pull(username, backup_dir, repo):
+    print "Backing up %s..." % repo['name']
+    print "* %s" % repo['description']
+    repo_dir = '/'.join([backup_dir, repo['name']])
     if os.path.exists(repo_dir):
         call(['git', 'pull'], cwd=repo_dir)
     else:
         clone_url = CLONE_URL % {'username': username,
-                                 'repo_name': repo_name}
+                                 'repo_name': repo['name']}
         call(['git', 'clone', clone_url], cwd=backup_dir)
     print
 
@@ -34,16 +34,13 @@ def main():
     if not os.path.exists(backup_dir):
         os.makedirs(backup_dir)
 
-    tree = ElementTree()
-    tree.parse(urllib.urlopen(REPO_LIST_API % {'username': username}))
-    repos = tree.findall('repository/name')
-    for repo in repos:
-        repo_name = repo.text
-        clone_or_pull(username, backup_dir, repo_name)
+    j = json.load(urllib.urlopen(REPO_LIST_API % {'username': username}))
+    repos = j['repositories']
+    for repo in sorted(repos, key=lambda x: x['name']):
+        clone_or_pull(username, backup_dir, repo)
 
 if __name__ == '__main__':
     if len(sys.argv) == 3:
         main()
     else:
         usage()
-
